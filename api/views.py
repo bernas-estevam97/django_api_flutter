c12575853aaeee6fbed05161b91f83728f8699bb
ObliviouZ <bernaz.10golden97@gmail.com>
2022-10-11 04:49:03 +0100

    D.F
    Django API + FLUTTER

---
+++ api/views.py
@@ -0,0 +1,38 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/notes/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of notes'
+        },
+        {
+            'Endpoint': '/notes/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single note object'
+        },
+        {
+            'Endpoint': '/notes/create',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates new notes with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/update',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Creates an existing note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/delete',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes an existing note'
+        },
+    ]
+    return Response(routes)
